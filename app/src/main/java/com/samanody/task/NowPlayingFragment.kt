2d017b22de3cf3523818bb91d24f82b63052e8f1
Ahmed Samanody <a.esam997@gmail.com>
2025-04-26 02:29:26 +0300

    fix: update UI strings for  localization support

--- app/src/main/java/com/samanody/task/NowPlayingFragment.kt
+++ app/src/main/java/com/samanody/task/NowPlayingFragment.kt
@@ -2,9 +2,7 @@ package com.samanody.task
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
